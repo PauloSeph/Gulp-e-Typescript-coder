b81e2eb934e015a2e8f11a470c2214e64d8a9b46
Seph <paulo.seph@hotmail.com>
2019-12-14 11:05:13 -0300

    Atualizando o deps.js

--- spa/gulpTasks/deps.js
+++ spa/gulpTasks/deps.js
@@ -2,17 +2,27 @@
 // por exemplo as fonts
 
 // Também definir as estruturas basicas
+
 const gulp = require('gulp')
+const uglifycss = require ('gulp-uglifycss')
+const concat = require('gulp-concat')
 
 gulp.task('deps', ['deps.css', 'deps.fonts'])
 
 
 
 gulp.task('deps.css', () => {
-    
+    return gulp.src([
+        'node_modules/font-awesome/css/font-awesome.min.css'
+    ])
+        .pipe(uglifycss({"uglyComments": false}))
+        .pipe(concat('deps.min.css'))
+        .pipe(gulp.dest('build/assets/css'))
 })
-
-
+ 
+// copiando as fonts que estão dentro do font-awesome para a pasta build
+// vai pegar todos arquivos que estão dentro do caminho passado da pasta fonts
 gulp.task('deps.fonts', () => {
-    
+    return gulp.src(['node_modules/font-awesome/fonts/*.*'])
+    .pipe(gulp.dest('build/assets/fonts'))
 })
